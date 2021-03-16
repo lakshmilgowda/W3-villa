8e677d61951f4cb4d6587e843d324b17e7b9ccb8
lakshmilgowda <65533664+lakshmilgowda@users.noreply.github.com>
2021-03-16 09:50:06 +0530

    quiz

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The term "DFD" stands for?",
+    answer: "Data flow diagram",
+    options: [
+      "Data file diagram",
+      "Data flow document",
+      "Data flow diagram",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The term "SQL" stands for?",
+    answer: "Structured query language",
+    options: [
+      "Standard query language",
+      "Sequential query language",
+      "Structured query language",
+      "Server-side query language"
+    ]
+  },
+    {
+    numb: 3,
+    question: " What is the relation calculus?",
+    answer: "It is a non-procedural language",
+    options: [
+      "It is a kind of procedural language",
+      "It is a non-procedural language",
+      "It is a high-level language",
+      "It is Data Definition language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The term "ODBC" stands for_____?",
+    answer: "Open database connectivity",
+    options: [
+      "Oral database connectivity",
+      "Oracle database connectivity",
+      "Open database connectivity",
+      "Object database connectivity"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following levels is considered as the level closed to the end-users?",
+    answer: "External Level",
+    options: [
+      "Internal Level",
+      "External Level",
+      "Conceptual Level",
+      "Physical Level"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
